8e2613c1befe4ce770c95c55631d8fc391b4544d
hjj <814549124@qq.com>
2015-12-14 05:57:31 +0000

    修改测试中的设备token值,因为其值只能未数字

--- lib/xinge_app/xinge_app.rb
+++ lib/xinge_app/xinge_app.rb
@@ -8,6 +8,7 @@ class XingeApp
 
 	def push_android_by_token(title,content,dev_token)
 		params = android_params(title,content,dev_token)
+		puts params
 		BaseRequest.post('push/single_device',params,@secret_key)
 	end
 
@@ -15,13 +16,48 @@ class XingeApp
 	def android_params(title,content,dev_token)
 		params = {
 			access_id: @access_id,
-			dev_token: dev_token,
+			device_token: dev_token,
 			message_type: 1,
 			message: {
 				title: title,
 				content: content,
 				builder_id: 0,
 			}.to_json
+# 			message: '{
+# "title ":"xxx",
+# "content ":"xxxxxxxxx", 
+# "accept_time": 
+# [ {
+# “start”:{“hour”:”13”,“min”:”00”},
+# ”end”: {“hour”:”14”,“min”:”00”} },
+# {
+# “start”:{“hour”:”00”,”min”:”00”},
+# ”end”: {“hour”:”09”,“min”:”00”} }
+# ],
+# "n_id":0,  "builder_id":0, 
+# "ring":1, 
+# "ring_raw":"ring", 
+# "vibrate":1, 
+# "lights":1
+# "clearable":1, 
+# "icon_type":0 
+# "icon_res":"xg",
+# "action":{ 
+# "action_type ": 1, 
+#        "activity ": "xxx"
+# "aty_attr ":  {
+# "if":0, 
+# Intent.FLAG_ACTIVITY_RESET_TASK_IF_NEEDED);
+# "pf":0, 
+# }
+# "browser": {"url": "xxxx ","confirm": 1},
+#        “intent”: “xxx”
+#      },
+# "custom_content":{
+#        "key1": "value1",
+#        "key2": "value2"
+#      }
+# }'
 		}
 	end
 end
--- test/test_push_to_android.rb
+++ test/test_push_to_android.rb
@@ -2,13 +2,15 @@ require 'minitest/autorun'
 
 require 'xinge_app/xinge_app'
 
-class XingeAppTest < Minitest::Test
+class PushToAndroidTest < Minitest::Test
 	def test_base
 		assert XingeApp.new 2100163826,'6d460a9b2c394801e167b2c1dc2873f4'
 	end
+
 	def test_push_to_android
 		xinge_app = XingeApp.new 2100163826,'6d460a9b2c394801e167b2c1dc2873f4'
-		re = xinge_app.push_android_by_token('title','content','某token')
-		assert 40,re['ret_code']
+		re = xinge_app.push_android_by_token('title','content','4455654')
+		puts re
+		assert_equal 40,re['ret_code']
 	end
 end
